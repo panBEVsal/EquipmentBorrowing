5132d290022b1996147245b56395d43753d03559
Cajetas <beverlynicolecajetas8@gmail.com>
2026-08-28 00:43:46 +0800

    git add . git commit -m "Add in-memory repository"

---
+++ src/EquipmentBorrowing.Infrastructure/Repositories/InMemoryEquipmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using EquipmentBorrowing.Application.Interfaces;
+using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Infrastructure.Repositories;
+
+public class InMemoryEquipmentRepository : IEquipmentRepository
+{
+    private readonly List<Equipment> _equipment;
+
+    public InMemoryEquipmentRepository(IEnumerable<Equipment> seedData)
+    {
+        _equipment = seedData.ToList();
+    }
+
+    public Task<Equipment?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var item = _equipment.FirstOrDefault(e => e.Id == id);
+        return Task.FromResult(item);
+    }
+}   
---
+++ src/EquipmentBorrowing.Infrastructure/Repositories/InMemoryStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using EquipmentBorrowing.Application.Interfaces;
+using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Infrastructure.Repositories;
+
+public class InMemoryStudentRepository : IStudentRepository
+{
+    private readonly List<Student> _students;
+
+    public InMemoryStudentRepository(IEnumerable<Student> seedData)
+    {
+        _students = seedData.ToList();
+    }
+
+    public Task<Student?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var student = _students.FirstOrDefault(s => s.Id == id);
+        return Task.FromResult(student);
+    }
+}   
